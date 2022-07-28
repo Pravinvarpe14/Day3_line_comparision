50e0bc44954ebe58ff57d4f1dc062be6196e5f45
Pravin Varpe <pravinvarpe14@gmail.com>
2022-07-28 22:00:24 +0530

    added welcome message of line comparision programme

---
+++ src/line_comparision.java
@@ -0,0 +1,16 @@
+/*
+ * line comparision problem
+ */
+public class line_comparision {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * printing messaage
+		 */
+	System.out.println("Welcome to Line comparision computation");
+				
+
+	}
+
+}
