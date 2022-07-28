1f3d570147a73b3c54077718c85c7eda66e7b83a
Pravin Varpe <pravinvarpe14@gmail.com>
2022-07-28 22:18:51 +0530

    added comparision of two line programme

--- src/line_comparision.java
+++ src/line_comparision.java
@@ -5,32 +5,56 @@ public class line_comparision {
 
 	public static void main(String[] args) {
 		
-		/*
-		 * printing messaage
-		 */
-	System.out.println("Welcome to Line comparision computation");
-	
-	/*
-	 * taking variable 			
-	 */
-
-		int x1 = 1;
-		int x2 = 2;
-		int y1 = 3;
-		int y2 = 4;
+			/*
+			 * printing messaage
+			 */
+		System.out.println("Welcome to Line comparision computation");
 		
-		System.out.println("Coordinates x1 and y1 of first point is " + x1 + ", " + y1);
-		System.out.println("Coordinates x2 and y2 of second point is " + x2 + ", " + y2);
-	
 		/*
-		 * by using math.sq finding lnght of line one
+		 * taking variable 			
 		 */
-				double length1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+	
+			int x1 = 1;
+			int x2 = 2;
+			int y1 = 3;
+			int y2 = 4;
+			
+			System.out.println("Coordinates x1 and y1 of first point is " + x1 + ", " + y1);
+			System.out.println("Coordinates x2 and y2 of second point is " + x2 + ", " + y2);
+		
+				/*
+				 * by using math.sq finding lenght of line one
+				 */
+					double length1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 				
-				System.out.println("Length pf line1 is " + length1);
-	
-	
-	
+				System.out.println("Length of line1 is " + length1);
+				/*
+				 * line2 variables
+				 */
+				int x3 = 5;
+				int x4 = 6;
+				int y3 = 7;
+				int y4 = 8;
+				
+				System.out.println("Coordinates x1 and y1 of first point is " + x1 + ", " + y1);
+				System.out.println("Coordinates x2 and y2 of second point is " + x2 + ", " + y2);
+				
+				double length2 = Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2));
+				
+				System.out.println("Length of line1 is " + length2);
+				
+				/*
+				 * using if else statement to verifying the condition
+				 */
+				
+				 if(length1 == length2)
+			        {
+			            System.out.println("lines are equal");
+			        }
+			        else
+			            System.out.println("lines are not equal");
+				
+				
 	}
 
 }
